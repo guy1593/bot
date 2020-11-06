ebe94b3982eac91bb2ea2aef1594d166df1350b5
guy1593 <34449921+guy1593@users.noreply.github.com>
2020-11-07 01:50:39 +0200

    Update ICUP.py

--- ICUP.py
+++ ICUP.py
@@ -90,4 +90,4 @@ async def on_message(message):
         print(f'''User: {message.author} tried to do command {message.content} in channel {message.channel}''')
 
 
-client.run(process.env.BOT_TOKEN)
+client.run(BOT_TOKEN)
