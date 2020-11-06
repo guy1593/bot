713648ff836cbade37d0dc6d317cb3bfe77fcfb4
guy1593 <34449921+guy1593@users.noreply.github.com>
2020-11-07 01:27:02 +0200

    Create package.py

---
+++ package.py
@@ -0,0 +1,15 @@
+{
+  "name": "icup",
+  "description": "cup",
+  "version": "0.0.0",
+  "main": "ICUP.py"
+  "scripts": {
+    "start": "node ICUP.py"
+   },
+   "dependencies": {
+    "discord.py"
+    "re"
+    "urllib.request"
+    "random"
+   }
+ }
